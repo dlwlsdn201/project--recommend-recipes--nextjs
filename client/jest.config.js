3f7f29c5f974a2cb8438855ceb4e567a5b352549
JinWoo_Lee <dlwlsdn201@naver.com>
2023-05-24 23:54:14 +0900

    :wrench: jest 환경설정 수정

--- client/jest.config.js
+++ client/jest.config.js
@@ -30,8 +30,10 @@ module.exports = {
 	transform: {
 		// Use babel-jest to transpile tests with the next/babel preset
 		// https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
-		// '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
-		'\\.[jt]sx?$': 'ts-jest',
+		// '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+		// '\\.[jt]sx?$': 'ts-jest',
+		// '^.+\\.tsx?$': 'ts-jest',
+		'^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
 		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
 			'<rootDir>/fileTransformer.js'
 	},
