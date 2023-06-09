d97df110b45690037ecdea1a99adad7a21745ddc
JinWoo_Lee <dlwlsdn201@naver.com>
2023-06-09 22:24:33 +0900

    :lipstick: 레시피 추천 API 호출 시, Loading state 업데이트 및 Loading spin UI 애니메이션 동작 로직 구현

--- client/components/Home/index.tsx
+++ client/components/Home/index.tsx
@@ -8,22 +8,53 @@ import {
 import TextInput from '../Modules/TextInput';
 import { mainStore } from '@/source/store';
 import Primary from '../Modules/Buttons/Primary';
+import Loading from '../Modules/Loading';
 
 const MainComponent = () => {
-	const { setSpicyFilter, setMenuTypeFilter, setConceptFilter, setMaterials } =
-		mainStore();
-
+	const {
+		material1,
+		material2,
+		material3,
+		material4,
+		spicyFilter,
+		menuTypeFilter,
+		conceptFilter,
+		setSpicyFilter,
+		setMenuTypeFilter,
+		setConceptFilter,
+		setMaterials,
+		loading,
+		setLoading
+	} = mainStore();
+	// Chat GPT OpenAI API 요청 함수
 	const onFinishForm = async () => {
 		try {
 			console.log('추천 받기');
+			setLoading(true);
+			// 사용자 입력 값 전달 및 응답 받기
+			const inputValues = {
+				spicyFilter,
+				menuTypeFilter,
+				conceptFilter,
+				materials: [material1, material2, material3, material4]
+			};
+			const formattedUserInput = formatInputDataToRequest(inputValues);
+			const response = await CREATE_RECOMMEND_RECIPES({
+				userInput: formattedUserInput
+			});
+			if (response.status >= 200 && response.status < 300) {
+				console.log({ response });
+				setLoading(false);
+			}
 		} catch (error) {
+			setLoading(false);
 			console.error(error);
 		}
 	};
 
 	return (
 		<div className='root-container h-[100%] border-red-400 border-2 p-2'>
-			<div className='inner-container h-[100%] grid gap-6 tablet:grid-cols-5 grid-cols-1 grid-rows-1 border-blue-300 border-2'>
+			<div className='inner-container grid gap-6 tablet:grid-cols-5 grid-cols-1 grid-rows-1 border-blue-300 border-2'>
 				<div className='area--left border-green-400 border-2 col-span-2 p-8 flex items-start'>
 					<div className='inner-wrapper h-fit'>
 						<div className='title mobile:text-lg laptop:text-3xl'>
@@ -50,87 +81,88 @@ const MainComponent = () => {
 						</div>
 					</div>
 				</div>
-				<div className='area--right border-yellow-400 border-2 col-span-3 p-8'>
-					<div className='inner-wrapper grid grid-rows-12 grid-cols-1 gap-8'>
-						<div className='filters--wrapper grid row-start-1 tablet:grid-cols-3 mobile:grid-cols-1 gap-5'>
-							<div className='filter--block'>
-								<div className='label'>맵기 정도</div>
-								<div className='filter'>
-									<Select
-										options={spicyFilterItems}
-										isExistAll
-										onChange={(value) => {
-											console.log('value:', value);
-											setSpicyFilter(value);
-										}}
-									/>
+				<div className='area--right border-yellow-400 border-2 col-span-3'>
+					<Loading spinning={loading}>
+						<div className='inner-wrapper grid grid-rows-12 grid-cols-1 gap-8 p-8'>
+							<div className='filters--wrapper grid row-start-1 tablet:grid-cols-3 mobile:grid-cols-1 gap-5'>
+								<div className='filter--block'>
+									<div className='label'>맵기 정도</div>
+									<div className='filter'>
+										<Select
+											options={spicyFilterItems}
+											isExistAll
+											onChange={(value) => {
+												setSpicyFilter(Number(value));
+											}}
+										/>
+									</div>
+								</div>
+								<div className='filter--block'>
+									<div className='label'>종류</div>
+									<div className='filter'>
+										<Select
+											options={menuTypeFilterItems}
+											isExistAll
+											onChange={(value) => setMenuTypeFilter(Number(value))}
+										/>
+									</div>
+								</div>
+								<div className='filter--block'>
+									<div className='label'>목적</div>
+									<div className='filter'>
+										<Select
+											options={conceptFilterItems}
+											isExistAll
+											onChange={(value) => setConceptFilter(Number(value))}
+										/>
+									</div>
 								</div>
 							</div>
-							<div className='filter--block'>
-								<div className='label'>종류</div>
-								<div className='filter'>
-									<Select
-										options={menuTypeFilterItems}
-										isExistAll
-										onChange={setMenuTypeFilter}
-									/>
+							<div className='filters--wrapper grid row-start-2 grid-cols-2 gap-5 justify-stretch'>
+								<div className='filter--block'>
+									<div className='label'>재료1</div>
+									<div className='filter'>
+										<TextInput
+											placeholder='입력'
+											onChange={(value) => setMaterials('material1', value)}
+										/>
+									</div>
+								</div>
+								<div className='filter--block'>
+									<div className='label'>재료2</div>
+									<div className='filter'>
+										<TextInput
+											placeholder='입력'
+											onChange={(value) => setMaterials('material2', value)}
+										/>
+									</div>
+								</div>
+								<div className='filter--block'>
+									<div className='label'>재료3</div>
+									<div className='filter'>
+										<TextInput
+											placeholder='입력'
+											onChange={(value) => setMaterials('material3', value)}
+										/>
+									</div>
+								</div>
+								<div className='filter--block'>
+									<div className='label'>재료4</div>
+									<div className='filter'>
+										<TextInput
+											placeholder='입력'
+											onChange={(value) => setMaterials('material4', value)}
+										/>
+									</div>
 								</div>
 							</div>
-							<div className='filter--block'>
-								<div className='label'>목적</div>
-								<div className='filter'>
-									<Select
-										options={conceptFilterItems}
-										isExistAll
-										onChange={setConceptFilter}
-									/>
+							<div className='filters--wrapper grid row-start-4 grid-cols-1'>
+								<div className='submit--block auto-cols-max'>
+									<Primary label='추천 받기' onSubmit={onFinishForm} />
 								</div>
 							</div>
 						</div>
-						<div className='filters--wrapper grid row-start-2 grid-cols-2 gap-5 justify-stretch'>
-							<div className='filter--block'>
-								<div className='label'>재료1</div>
-								<div className='filter'>
-									<TextInput
-										placeholder='입력'
-										onChange={(value) => setMaterials('material1', value)}
-									/>
-								</div>
-							</div>
-							<div className='filter--block'>
-								<div className='label'>재료2</div>
-								<div className='filter'>
-									<TextInput
-										placeholder='입력'
-										onChange={(value) => setMaterials('material2', value)}
-									/>
-								</div>
-							</div>
-							<div className='filter--block'>
-								<div className='label'>재료3</div>
-								<div className='filter'>
-									<TextInput
-										placeholder='입력'
-										onChange={(value) => setMaterials('material3', value)}
-									/>
-								</div>
-							</div>
-							<div className='filter--block'>
-								<div className='label'>재료4</div>
-								<div className='filter'>
-									<TextInput
-										placeholder='입력'
-										onChange={(value) => setMaterials('material4', value)}
-									/>
-								</div>
-							</div>
-						</div>
-						<div className='filters--wrapper grid row-start-4 grid-cols-1'>
-							<div className='submit--block auto-cols-max'>
-								<Primary label='추천 받기' onSubmit={onFinishForm} />
-							</div>
-						</div>
-					</div>
+					</Loading>
 				</div>
 			</div>
 		</div>
