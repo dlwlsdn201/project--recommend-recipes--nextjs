f22234dfe9a5cca5a94be52c744cac088ee7dbc2
JinWoo_Lee <dlwlsdn201@naver.com>
2023-06-19 09:33:42 +0900

    :wrench: jest config 업데이트(ResizeObserver 테스트 환경 세팅)

--- client/jest.setup.js
+++ client/jest.setup.js
@@ -1,2 +1,8 @@
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
+
+global.ResizeObserver = jest.fn(() => ({
+	observe: jest.fn(),
+	unobserve: jest.fn(),
+	disconnect: jest.fn()
+}));
