ab2822627aea99114d8ffc77d0500aebfc2bd0fa
JinWoo_Lee <dlwlsdn201@naver.com>
2023-07-21 21:08:32 +0900

    :cyclone: 코드 리팩터링

--- client/components/layout.tsx
+++ client/components/layout.tsx
@@ -1,12 +1,11 @@
-import React, { ReactNode } from 'react';
+import React,{ReactNode} from 'react';
 import Header from './header';
-import { useRouter } from 'next/router';
-import { TransitionGroup, Transition } from 'react-transition-group';
-
+// import { useRouter } from 'next/router';
+// import { TransitionGroup, Transition } from 'react-transition-group';
 interface IProps {
 	children: ReactNode;
 }
-const TIMEOUT = 100;
+const TIMEOUT = 150;
 const getTransitionStyles = {
 	entering: {
 		position: 'absolute',
@@ -23,30 +22,14 @@ const getTransitionStyles = {
 };
 
 const Layout = ({ children }: IProps) => {
-	const router = useRouter();
+	// const router = useRouter();
 
 	return (
 		<div className='relative h-[100%]'>
 			<Header />
-			<TransitionGroup style={{ position: 'relative' }}>
-				<Transition
-					key={router.pathname}
-					timeout={{
-						enter: TIMEOUT,
-						exit: TIMEOUT
-					}}>
-					{(status) => (
-						<div
-							style={{
-								...getTransitionStyles[status]
-							}}>
-							<main>
-								<div className='h-[100%] w-[100%] p-[2em] '>{children}</div>
-							</main>
-						</div>
-					)}
-				</Transition>
-			</TransitionGroup>
+			<main>
+				<div className='h-[100%] w-[100%] p-[2em] '>{children}</div>
+			</main>
 			{/* <footer>footer</footer> */}
 		</div>
 	);
