f103739bbafc91af0a8d378cd55c9e0dc79d9395
JinWoo_Lee <dlwlsdn201@naver.com>
2023-07-28 15:25:40 +0900

    :cyclone: 코드 리팩터링 2차

--- client/components/layout.tsx
+++ client/components/layout.tsx
@@ -4,52 +4,54 @@ import { useRouter } from 'next/router';
 import { TransitionGroup, Transition } from 'react-transition-group';
 
 interface IProps {
-	children: ReactNode;
+  children: ReactNode;
 }
 const TIMEOUT = 100;
 const getTransitionStyles = {
-	entering: {
-		position: 'absolute',
-		opacity: 0
-	},
-	entered: {
-		transition: `opacity ${TIMEOUT}ms ease-in-out, transform ${TIMEOUT}ms ease-in-out`,
-		opacity: 1
-	},
-	exiting: {
-		transition: `opacity ${TIMEOUT}ms ease-in-out, transform ${TIMEOUT}ms ease-in-out`,
-		opacity: 0
-	}
+  entering: {
+    position: 'absolute',
+    opacity: 0,
+  },
+  entered: {
+    transition: `opacity ${TIMEOUT}ms ease-in-out, transform ${TIMEOUT}ms ease-in-out`,
+    opacity: 1,
+  },
+  exiting: {
+    transition: `opacity ${TIMEOUT}ms ease-in-out, transform ${TIMEOUT}ms ease-in-out`,
+    opacity: 0,
+  },
 };
 
 const Layout = ({ children }: IProps) => {
-	const router = useRouter();
+  const router = useRouter();
 
-	return (
-		<div className='relative h-[100%]'>
-			<Header />
-			<TransitionGroup style={{ position: 'relative' }}>
-				<Transition
-					key={router.pathname}
-					timeout={{
-						enter: TIMEOUT,
-						exit: TIMEOUT
-					}}>
-					{(status) => (
-						<div
-							style={{
-								...getTransitionStyles[status]
-							}}>
-							<main>
-								<div className='h-[100%] w-[100%] p-[2em] '>{children}</div>
-							</main>
-						</div>
-					)}
-				</Transition>
-			</TransitionGroup>
-			{/* <footer>footer</footer> */}
-		</div>
-	);
+  return (
+    <div className="relative h-[100%]">
+      <Header />
+      <TransitionGroup style={{ position: 'relative' }}>
+        <Transition
+          key={router.pathname}
+          timeout={{
+            enter: TIMEOUT,
+            exit: TIMEOUT,
+          }}
+        >
+          {(status) => (
+            <div
+              style={{
+                ...getTransitionStyles[status],
+              }}
+            >
+              <main>
+                <div className="h-[100%] w-[100%] p-[2em] ">{children}</div>
+              </main>
+            </div>
+          )}
+        </Transition>
+      </TransitionGroup>
+      {/* <footer>footer</footer> */}
+    </div>
+  );
 };
 
 export default Layout;
