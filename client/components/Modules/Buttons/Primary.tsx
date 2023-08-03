39d14dce1ab5a2c9aebba84787bae19b60d51014
JinWoo_Lee <dlwlsdn201@naver.com>
2023-08-03 21:23:27 +0900

    :zap: submit 이벤트 시, 새로고침 증상 개선

--- client/components/Modules/Buttons/Primary.tsx
+++ client/components/Modules/Buttons/Primary.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 
 interface Props {
-	label?: string;
-	onSubmit?: Function;
-	testId: string;
-	disabled: boolean;
+  label?: string;
+  onSubmit?: Function;
+  testId: string;
+  disabled: boolean;
 }
 
 const Primary = ({ label, onSubmit, testId, disabled }: Props): JSX.Element => {
-	return (
-		<button
-			data-testid={testId}
-			onClick={() => onSubmit()}
-			disabled={disabled}
-			className='btn btn-xs w-[100%] btn-active btn-primary mobile:btn-sm tablet:btn-md laptop:btn-lg'>
-			{label ?? '실행'}
-		</button>
-	);
+  return (
+    <button
+      data-testid={testId}
+      onClick={() => onSubmit()}
+      disabled={disabled}
+      type="button"
+      className="btn btn-xs w-[100%] btn-active btn-primary mobile:btn-sm tablet:btn-md laptop:btn-lg"
+    >
+      {label ?? '실행'}
+    </button>
+  );
 };
 
 export default Primary;
