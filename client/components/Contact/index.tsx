eeb1c59a46c6323945d7c95c6c28c82acb400b18
JinWoo_Lee <dlwlsdn201@naver.com>
2023-08-03 21:21:53 +0900

    :zap: 메일 전송 submit 버튼 disabled 조건 로직 구현

--- client/components/Contact/index.tsx
+++ client/components/Contact/index.tsx
@@ -1,37 +1,55 @@
-import React, { ReactNode, useState } from 'react';
+import React, { useState } from 'react';
 import Name from './Inputs/Name';
 import Email from './Inputs/Email';
 import PhoneNumber from './Inputs/PhoneNumber';
 import Primary from '../Modules/Buttons/Primary';
+import Comment from './Inputs/Comment';
+import sendMail from './Modules/Mail';
 
-const Contact = ():ReactNode => {
-  const [name,setName] = useState(null);
-  const [email,setEmail] = useState(null);
-  const [phoneNumber,setPhoneNumber] = useState(null);
-	return (
-		<div
-			id='inner-container'
-			className='border-2 border-yellow-300 grid gap-y-8 py-6 px-32'>
-			<div id='title-wrapper' className='w-[100%] grid-cols-1'>
-				<h1 className='col-span-1 text-center text-2xl'>Contact Me</h1>
-			</div>
-			<form onSubmit={() => console.log('form 완성')}>
-				<div id='form-wrapper' className='grid grid-cols-1 gap-8'>
+const initialState = '';
+
+const Contact = (): React.ReactElement => {
+  const [name, setName] = useState(initialState);
+  const [email, setEmail] = useState(initialState);
+  const [phoneNumber, setPhoneNumber] = useState(initialState);
+  const [comment, setComment] = useState(initialState);
+
+  // input 초기화
+  const resetInputs = () => {
+    setName(initialState);
+    setEmail(initialState);
+    setPhoneNumber(initialState);
+    setComment(initialState);
+  };
+
+  const disabled: boolean = !name.length || !email.length || !phoneNumber.length || !comment.length;
+
+  return (
+    <div id="inner-container" className="border-2 border-yellow-300 grid gap-y-8 py-6 px-32">
+      <div id="title-wrapper" className="w-[100%] grid-cols-1">
+        <h1 className="col-span-1 text-center text-2xl">Contact Me</h1>
+      </div>
+      <form>
+        <div id="form-wrapper" className="grid grid-cols-1 gap-8">
           <Name value={name} setName={setName} />
           <Email value={email} setEmail={setEmail} />
           <PhoneNumber value={phoneNumber} setPhoneNumber={setPhoneNumber} />
-					<div id='grid-row' className='w-[37%]'>
-						<Primary
-							label='Send'
-							disabled={false}
-							onSubmit={() => console.log('전송')}
-							testId='submit-button'
-						/>
-					</div>
-				</div>
-			</form>
-		</div>
-	);
+          <Comment value={comment} setComment={setComment} />
+          <div id="grid-row" className="w-[50%]">
+            <Primary
+              label="Send"
+              disabled={disabled}
+              onSubmit={() => {
+                sendMail({ service: 'gmail', name, email, content: comment, phoneNumber });
+                resetInputs();
+              }}
+              testId="submit-button"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
