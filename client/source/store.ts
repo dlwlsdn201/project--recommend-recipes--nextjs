5b52650d0bad4b5da037dbc299e69aad5693fdec
JinWoo_Lee <dlwlsdn201@naver.com>
2023-08-05 18:43:08 +0900

    :lipstick: API fetch 성공/실패에 대한 커스텀 Alert UI 모듈 구현

--- client/source/store.ts
+++ client/source/store.ts
@@ -1,48 +1,48 @@
 import { create } from 'zustand';
-import {
-	INTERFACE_commonStore,
-	INTERFACE_loginStore,
-	INTERFACE_mainStore
-} from './types';
+import { INTERFACE_commonStore, INTERFACE_loginStore, INTERFACE_mainStore } from './types';
 
 // [store] 전역 데이터
 export const rootStore = (set) => ({});
 
 export const commonStore = create<INTERFACE_commonStore>((set) => ({
-	isLogin: false,
-	setIsLogin: (value: boolean) => set(() => ({ isLogin: value }))
+  isLogin: false,
+  setIsLogin: (value: boolean) => set(() => ({ isLogin: value })),
+  loading: false,
+  setLoading: (value: boolean) => set(() => ({ loading: value })),
+  isError: false,
+  setIsError: (value: boolean) => set(() => ({ loading: value })),
+  isFetched: false,
+  setIsFetched: (value: boolean) => set(() => ({ loading: value })),
 }));
 
 // [store] Login 페이지 데이터
 export const loginStore = create<INTERFACE_loginStore>((set) => ({
-	userId: undefined,
-	setUserId: (value: string) => set(() => ({ userId: value })),
-	password: undefined,
-	setPassword: (value: string) => set(() => ({ password: value }))
+  userId: undefined,
+  setUserId: (value: string) => set(() => ({ userId: value })),
+  password: undefined,
+  setPassword: (value: string) => set(() => ({ password: value })),
 }));
 
 export const mainStore = create<INTERFACE_mainStore>((set) => ({
-	spicyFilter: 0,
-	setSpicyFilter: (value: 0 | 1 | 2 | 3) => set(() => ({ spicyFilter: value })),
-	menuTypeFilter: 0,
-	setMenuTypeFilter: (value: 0 | 1 | 2 | 3 | 4) =>
-		set(() => ({ menuTypeFilter: value })),
-	conceptFilter: 0,
-	setConceptFilter: (value: 0 | 1 | 2 | 3 | 4 | 5 | 6) =>
-		set(() => ({ conceptFilter: value })),
-	material1: undefined,
-	material2: undefined,
-	material3: undefined,
-	// material4: undefined,
-	setMaterials: (key: string, value: string) => set(() => ({ [key]: value })),
-	loading: false,
-	setLoading: (value) => set(() => ({ loading: value })),
-	apiData: undefined,
-	setApiData: (value) => set(() => ({ apiData: value })),
-	modal: false,
-	setModal: (value) => set(() => ({ modal: value })),
-	isRequested: false,
-	setIsRequested: (value) => set(() => ({ isRequested: value }))
+  spicyFilter: 0,
+  setSpicyFilter: (value: 0 | 1 | 2 | 3) => set(() => ({ spicyFilter: value })),
+  menuTypeFilter: 0,
+  setMenuTypeFilter: (value: 0 | 1 | 2 | 3 | 4) => set(() => ({ menuTypeFilter: value })),
+  conceptFilter: 0,
+  setConceptFilter: (value: 0 | 1 | 2 | 3 | 4 | 5 | 6) => set(() => ({ conceptFilter: value })),
+  material1: undefined,
+  material2: undefined,
+  material3: undefined,
+  // material4: undefined,
+  setMaterials: (key: string, value: string) => set(() => ({ [key]: value })),
+  loading: false,
+  setLoading: (value) => set(() => ({ loading: value })),
+  apiData: undefined,
+  setApiData: (value) => set(() => ({ apiData: value })),
+  modal: false,
+  setModal: (value) => set(() => ({ modal: value })),
+  isRequested: false,
+  setIsRequested: (value) => set(() => ({ isRequested: value })),
 }));
 
 // const hookDevtools = (store) => create(devtools(store));
--- client/source/types.ts
+++ client/source/types.ts
@@ -1,33 +1,39 @@
 export interface INTERFACE_commonStore {
-	isLogin: boolean;
-	setIsLogin: Function;
+  isLogin: boolean;
+  setIsLogin: Function;
+  loading: boolean;
+  setLoading: Function;
+  isFetched: boolean;
+  setIsFetched: Function;
+  isError: boolean;
+  setIsError: Function;
 }
 
 export interface INTERFACE_loginStore {
-	userId: string;
-	setUserId: Function;
-	password: string;
-	setPassword: Function;
+  userId: string;
+  setUserId: Function;
+  password: string;
+  setPassword: Function;
 }
 
 export interface INTERFACE_mainStore {
-	spicyFilter: 0 | 1 | 2 | 3;
-	setSpicyFilter: Function;
-	menuTypeFilter: 0 | 1 | 2 | 3 | 4;
-	setMenuTypeFilter: Function;
-	conceptFilter: 0 | 1 | 2 | 3 | 4 | 5 | 6;
-	setConceptFilter: Function;
-	material1: string;
-	material2: string;
-	material3: string;
-	// material4: string;
-	setMaterials: Function;
-	loading: boolean;
-	setLoading: Function;
-	apiData: string;
-	setApiData: Function;
-	modal: boolean;
-	setModal: Function;
-	isRequested: boolean;
-	setIsRequested: Function;
+  spicyFilter: 0 | 1 | 2 | 3;
+  setSpicyFilter: Function;
+  menuTypeFilter: 0 | 1 | 2 | 3 | 4;
+  setMenuTypeFilter: Function;
+  conceptFilter: 0 | 1 | 2 | 3 | 4 | 5 | 6;
+  setConceptFilter: Function;
+  material1: string;
+  material2: string;
+  material3: string;
+  // material4: string;
+  setMaterials: Function;
+  loading: boolean;
+  setLoading: Function;
+  apiData: string;
+  setApiData: Function;
+  modal: boolean;
+  setModal: Function;
+  isRequested: boolean;
+  setIsRequested: Function;
 }
