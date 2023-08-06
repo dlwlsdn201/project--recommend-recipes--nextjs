685523cff1134df5dfed390c428c95d03a99d526
JinWoo_Lee <dlwlsdn201@naver.com>
2023-08-06 17:05:27 +0900

    :wrench: organization key 재발급

--- server/server.js
+++ server/server.js
@@ -7,9 +7,10 @@ const bodyParser = require('body-parser');
 const { Configuration, OpenAIApi } = require('openai');
 
 const apiKey = process.env.OPENAPI_API_KEY;
+const organizationKey = process.env.OPENAPI_ORGANIZATION_ID;
 const configuration = new Configuration({
-	organization: 'org-UTqoP6O0CK7qqnFxn9CNHmhi',
-	apiKey
+  organization: organizationKey,
+  apiKey,
 });
 const openai = new OpenAIApi(configuration);
 
@@ -20,21 +21,21 @@ app.use(cors());
 
 // Set up the ChatGPT endpoint
 app.post('/chat', async (req, res) => {
-	// Get the prompt from the request
-	const { userInput } = req.body;
+  // Get the prompt from the request
+  const { userInput } = req.body;
 
-	// Generate a response with ChatGPT
-	const completion = await openai.createCompletion({
-		model: 'text-davinci-003',
-		prompt: userInput,
-		max_tokens: 700,
-		temperature: 0.2
-	});
-	res.send(completion.data);
+  // Generate a response with ChatGPT
+  const completion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: userInput,
+    max_tokens: 700,
+    temperature: 0.2,
+  });
+  res.send(completion.data);
 });
 
 // Start the server
 const port = 8080;
 app.listen(port, () => {
-	console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
