a638415a9dd6803f82b1da617702b8243f52a12a
JinWoo_Lee <dlwlsdn201@naver.com>
2026-03-02 12:43:45 +0900

    ✨ #14 AI API 마이그레이션 (`openAI API` -> `Gemini API`)
    - 정책 변경으로 인해 더이상 무료 API 지원을 하지 않는 openAI 의 API을 Google Gemini API으로 변경

--- server/server.js
+++ server/server.js
@@ -4,38 +4,15 @@ const dotenv = require('dotenv');
 dotenv.config();
 const bodyParser = require('body-parser');
 
-const { Configuration, OpenAIApi } = require('openai');
-
-const apiKey = process.env.OPENAPI_API_KEY;
-const organizationKey = process.env.OPENAPI_ORGANIZATION_ID;
-console.log('@@@TEST@@@ ==>', apiKey);
-const configuration = new Configuration({
-  organization: organizationKey,
-  apiKey,
-});
-const openai = new OpenAIApi(configuration);
-
-// Set up the server
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// Set up the ChatGPT endpoint
-app.post('/chat', async (req, res) => {
-  // Get the prompt from the request
-  const { userInput } = req.body;
-
-  // Generate a response with ChatGPT
-  const completion = await openai.createCompletion({
-    model: 'text-davinci-003',
-    prompt: userInput,
-    max_tokens: 700,
-    temperature: 0.2,
-  });
-  res.send(completion.data);
+// Basic health check or root route
+app.get('/', (req, res) => {
+  res.send('Recipe Recommendation Server is running');
 });
 
-// Start the server
 const port = 8080;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
