c843c4f03fc5d0c3ea61ed20ef1af3db8680e588
JinWoo_Lee <dlwlsdn201@naver.com>
2026-03-03 22:37:43 +0900

    :art: #14 SNS 로그인 UI 요소 레이아웃 및 로고 컬러 보완

--- components/Modules/Icons.tsx
+++ components/Modules/Icons.tsx
@@ -1,13 +1,11 @@
-import { SiKakaotalk, SiGoogle } from 'react-icons/si';
+import { SiKakaotalk } from 'react-icons/si';
+import { FcGoogle } from 'react-icons/fc';
 
 // === [Login page] ===
-export const SNS_KAKAO = (
-	iconSize?: number | string,
-	style?
-): React.ReactElement => (
-	<SiKakaotalk size={iconSize ?? '2rem'} style={{ margin: 0, ...style }} />
+export const SNS_KAKAO = (iconSize?: number | string, style?: React.CSSProperties): React.ReactElement => (
+  <SiKakaotalk size={iconSize ?? '1.75rem'} />
 );
 
-export const SNS_GOOGLE = (iconSize?: number | string): React.ReactElement => (
-	<SiGoogle size={iconSize ?? '2rem'} />
+export const SNS_GOOGLE = (iconSize?: number | string, style?: React.CSSProperties): React.ReactElement => (
+  <FcGoogle size={iconSize ?? '1.75rem'} style={{ ...style }} />
 );
